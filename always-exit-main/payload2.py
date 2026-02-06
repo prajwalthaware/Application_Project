5b3f8fa8c257187b8b784c7ea9539e4d6291ccac
Prajwal <prajwalthaware@gmail.com>
2026-02-06 15:27:44 +0530

    clean

--- always-exit-main/payload2.py
+++
@@ -1,6 +0,0 @@
-# Create a module that prints the flag when imported
-import os
-import sys
-
-# This code runs AT IMPORT TIME, before sys.exit()
-os.system('cat /app/flag.txt')
--- always-exit-main/payload4.py
+++
@@ -1,3 +0,0 @@
-# Comment continuation - this won't work
-
-# But wait, what if we create a string literal that continues?
